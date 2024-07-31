ca4042139583cef185415635daadfdf55ee673c8
Vic428-human <z0983195379a@gmail.com>
2024-08-01 03:09:09 +0800

    預先定義好pre跟post的鉤子時機，但暫時只有先用到pre save

--- chromeapi/Api/Model/userModel.js
+++ chromeapi/Api/Model/userModel.js
@@ -5,14 +5,14 @@ function validator(val) {
   return val === this.password;
 }
 
-//設計資料格式
+//模式定义:主要指定对应的MongoDB集合的字段和字段类型。
 const userSchema = new mongoose.Schema({
   //   name: String,
   name: {
     type: String,
     required: [true, "請輸入你的名字"],
   },
-  username: {
+  email: {
     type: String,
     required: [true, "請輸入你的信箱"],
     unique: true,
@@ -32,20 +32,68 @@ const userSchema = new mongoose.Schema({
   mnemonic: String,
 });
 
-// TODO:
-userSchema.pre("save", async function () {
-  await doStuff();
-  await doMoreStuff();
+//pre钩子注册：
+userSchema.pre("init", (doc) => {
+  console.log("pre-init");
+});
+userSchema.pre("validate", (next) => {
+  console.log("validate");
+  next();
 });
 
-//準備一個模型
+userSchema.pre("save", async function (next) {
+  console.log("save");
+  next();
+});
+
+userSchema.pre("find", function (next) {
+  console.log("find");
+  next();
+});
+
+//post钩子注册：
+studentSchema.post("init", (doc) => {
+  console.log("post-init");
+});
+
+studentSchema.post("validate", (doc, next) => {
+  console.log("post-validate");
+  next();
+});
+
+studentSchema.post("save", function (doc, next) {
+  console.log("post-save");
+  next();
+});
+
+studentSchema.post("find", function (doc, next) {
+  console.log("post-find");
+  next();
+});
+
+//模型创建：将模式编译成模型
 const Token = mongoose.model("Token", userSchema);
 
 module.exports = Token;
 
-// 模板創建一個新的記錄
-// const accountOne = new Account({
-//   privateKey: "privateKeyOne",
-//   address: "addressOne",
+//save方法引起的相关已注册的钩子的执行
+// let tokenObj = new Token({
+//   name: "測試人員",
+//   email: "abc@gmail.com",
+//   password: 123456,
+//   passwordComfirm: 123456,
 // });
-// console.log(accountOne.privateKey); // 'privateKey'
+
+// console.log(tokenObj.passwordComfirm);
+
+tokenObj.save((err, doc) => {
+  if (err) throw err;
+  console.log("1.使用实例save()方法保存成功！");
+});
+
+//find方法引起的相关已注册的钩子的执行
+tokenObj.find((err, Token) => {
+  if (err) throw err;
+  console.log("查询所有Token结果如下：");
+  console.log(Token.toString());
+});
