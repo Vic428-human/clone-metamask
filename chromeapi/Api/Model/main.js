ab6c5f8d5804821c6b2080d03daec0c84b0ef4a3
Vic428-human <z0983195379a@gmail.com>
2024-08-01 03:00:52 +0800

    優化 連接到你的資料庫

--- chromeapi/Api/Model/main.js
+++ chromeapi/Api/Model/main.js
@@ -1,11 +1,29 @@
+/**
+ * MongoDB数据库操作:mongoose对象模型库之中间件pre和post钩子
+ */
 const mongoose = require("mongoose");
 
 // 連接到你的資料庫
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  //与数据库建立连接
+  // await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  mongoose.connect("mongodb://127.0.0.1:27017/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const conn = mongoose.connection;
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  conn.on("error", (err) => {
+    console.error("数据库连接失败：" + err);
+  });
+  conn.once("open", () => {
+    console.log("数据库连接成功！");
+  });
+
+  // 做法2: if your database has auth enabled
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');`
 }
+
 exports.main = main;
 
 // main().catch((err) => console.log(err));
